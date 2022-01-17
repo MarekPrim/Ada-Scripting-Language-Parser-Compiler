b20fc3f32668c8df760612a7684f768238d83465
MarekPrim <kilyanlegallic@gmail.com>
2022-01-17 17:36:52 +0100

    variables ads et adb

--- src/interpreteur/variables.ads
+++ src/interpreteur/variables.ads
@@ -1,12 +1,27 @@
+with Ada.Strings.Unbounded;
+use Ada.Strings.Unbounded;
+
 generic
 type T is private
 package variables is
 
     type T_Variable is record
         valeurVariable : T;
-        typeVariable : Chaine;
-        nomVariable : Chaine;
+        typeVariable : Unbounded_String;
+        nomVariable : Unbounded_String;
         isConstant : Boolean;
     end record;
+
+    function getValeur(variable : in T_Variable) return T;
+    procedure setValeur(variable : in out T_Variable; valeur : in T);
+
+    function getType(variable : in T_Variable) return Unbounded_String;
+    procedure setType(variable : in out T_Variable; typeVariable : in Unbounded_String);
+
+    function getNom(variable : in T_Variable) return Unbounded_String;
+    procedure setNom(variable : in out T_Variable; nomVariable : in Unbounded_String);
+    
+    function isConstant(variable : in T_Variable) return Boolean;
+
 
 end variables;
