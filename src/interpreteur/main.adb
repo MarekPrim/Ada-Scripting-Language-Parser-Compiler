0135aaf5ec4bd849ee7289a37cdb1d19daee64de
EtienneN10 <etienne10.ngolo@gmail.com>
2022-01-25 10:44:52 +0100

    ali o out

--- src/interpreteur/main.adb
+++
@@ -1,8 +0,0 @@
-with Ada.Text_IO, Ada.Integer_Text_IO, intermediaire;
-use Ada.Text_IO, Ada.Integer_Text_IO, intermediaire;
-
-procedure main is
-   fileName : constant string := "facto.med";
-begin
-   traiterProgramme(fileName);
-end main;
