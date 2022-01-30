6fefd946eaf5df9bc14af5a3fd986302a3f35f64
MarekPrim <59203712+MarekPrim@users.noreply.github.com>
2022-01-30 17:44:09 +0100

    commentaire

--- src/interpreteur/intermediaire.adb
+++ src/interpreteur/intermediaire.adb
@@ -1,6 +1,10 @@
-with ada.Text_IO, ada.integer_Text_IO, Ada.Strings.Unbounded, Ada.Text_IO.Unbounded_IO, Ada.Characters.Handling, operateurs, operations, variables, operations_liste, manipulation_chaine;
-use ada.Text_IO, ada.integer_Text_io, Ada.Strings.Unbounded, Ada.Text_IO.Unbounded_IO, Ada.Characters.Handling, operateurs, operations, variables, operations_liste, manipulation_chaine;
-with Ada.Command_Line; use Ada.Command_Line;
+with ada.Text_IO, ada.integer_Text_IO, Ada.Strings.Unbounded,
+   Ada.Text_IO.Unbounded_IO, Ada.Characters.Handling, operateurs, operations,
+   variables, operations_liste, manipulation_chaine;
+use ada.Text_IO, ada.integer_Text_io, Ada.Strings.Unbounded,
+   Ada.Text_IO.Unbounded_IO, Ada.Characters.Handling, operateurs, operations,
+   variables, operations_liste, manipulation_chaine;
+with Ada.Command_Line;  use Ada.Command_Line;
 with Ada.IO_EXCEPTIONS; use Ada.IO_EXCEPTIONS;
 
 package body intermediaire is
@@ -12,30 +16,31 @@ package body intermediaire is
 
     procedure traiter_programme is
 
-        variables : T_List_Variable;
-        instructions : T_List_Instruction;
+        variables      : T_List_Variable;
+        instructions   : T_List_Instruction;
         l_instructions : T_List_Instruction;
-        choice : Integer;
-        fileName : Unbounded_String;
-    
+        choice         : Integer;
+        fileName       : Unbounded_String;
+
     begin
 
         begin
             loop
-                fileName := To_Unbounded_String(Argument(1));
-            exit when (Argument_Count /= 0);
+                fileName := To_Unbounded_String (Argument (1));
+                exit when (Argument_Count /= 0);
             end loop;
 
-            exception
-            when Constraint_Error => Put_Line("Vous n'avez pas renseigné le fichier à interpréter;");
-            Put_Line("Utilisation du programme : ./main ./chemin_vers_fichier/fichier_souhaité");
-            raise Program_Error;
+        exception
+            when Constraint_Error =>
+                Put_Line
+                   ("Vous n'avez pas renseigné le fichier à interpréter;");
+                Put_Line
+                   ("Utilisation du programme : ./main ./chemin_vers_fichier/fichier_souhaité");
+                raise Program_Error;
         end;
-        
-        if(Argument_Count = 2) then
-            choice := (if Argument(2) = "--DEBUG"
-            then 1
-            else 0);
+
+        if (Argument_Count = 2) then
+            choice := (if Argument (2) = "--DEBUG" then 1 else 0);
         else
             choice := 0;
         end if;
@@ -43,28 +48,29 @@ package body intermediaire is
         skip_line;
 
         begin
-            parse_file(To_String(fileName), variables, instructions);
+            parse_file (To_String (fileName), variables, instructions);
 
-            exception
-            when Ada.IO_EXCEPTIONS.DEVICE_ERROR => Put_Line("Fichier inexistant/invalide");
+        exception
+            when Ada.IO_EXCEPTIONS.DEVICE_ERROR =>
+                Put_Line ("Fichier inexistant/invalide");
         end;
 
-        pointer_en_tete_instructions(instructions);
+        pointer_en_tete_instructions (instructions);
 
         l_instructions := instructions;
 
-        while(l_instructions /= null) loop
-            if(choice = 1 and l_instructions.next /= null) then
-                Put("Numéro de l'instruction en cours : ");
-                Put(l_instructions.all.ptrIns.all.numInstruction, 1);
+        while (l_instructions /= null) loop
+            if (choice = 1 and l_instructions.next /= null) then
+                Put ("Numéro de l'instruction en cours : ");
+                Put (l_instructions.all.ptrIns.all.numInstruction, 1);
                 new_line;
             end if;
-            interpreter_commande(l_instructions, variables);
+            interpreter_commande (l_instructions, variables);
         end loop;
 
-        afficher_liste(instructions);
-        Put_Line("Etat des variables en terminaison du programme");
-        afficher_liste(variables);
+        afficher_liste (instructions);
+        Put_Line ("Etat des variables en terminaison du programme");
+        afficher_liste (variables);
 
     end traiter_programme;
 
@@ -72,11 +78,14 @@ package body intermediaire is
     -- Les sous-programmes suivant ne sont pas déclarés private afin de pouvoir toujours réaliser des tests unitaires
     -- Dans une situation de mise en production, les autres sous-programmes devraient être déclarés private
 
-    procedure parse_file (fileName : in string; variables : in out T_List_Variable; instructions : in out T_List_Instruction) is
-        F         : File_Type;
+    procedure parse_file
+       (fileName     : in     string; variables : in out T_List_Variable;
+        instructions : in out T_List_Instruction)
+    is
+        F     : File_Type;
         ligne : Unbounded_string;
     begin
-    
+
         variables := creer_liste_vide;
 
         instructions := creer_liste_vide;
@@ -84,114 +93,168 @@ package body intermediaire is
         Open (F, In_File, fileName);    -- Ouverture du fichier en lecture
 
         loop
-            ligne := renvoyer_ligne_sans_espace(Ada.Text_IO.Unbounded_IO.get_line(f));     -- Lecture de la ligne courante sans espace
-        exit when (ligne_commence_par_mot_reserve(ligne, Reserved_Langage_Word'Image(Programme)));  -- Tant que la ligne ne commence par un mot réservé
+            ligne :=
+               renvoyer_ligne_sans_espace
+                  (Ada.Text_IO.Unbounded_IO.get_line
+                      (f));     -- Lecture de la ligne courante sans espace
+            exit when
+               (ligne_commence_par_mot_reserve
+                   (ligne,
+                    Reserved_Langage_Word'Image
+                       (Programme)));  -- Tant que la ligne ne commence par un mot réservé
         end loop;
 
         loop
-            ligne := renvoyer_ligne_sans_espace(Ada.Text_IO.Unbounded_IO.get_line(f)); -- Lecture de la ligne courante sans espace
-        exit when est_ligne_utile(ligne);
+            ligne :=
+               renvoyer_ligne_sans_espace
+                  (Ada.Text_IO.Unbounded_IO.get_line
+                      (f)); -- Lecture de la ligne courante sans espace
+            exit when est_ligne_utile (ligne);
         end loop;
 
-        if (ligne_commence_par_mot_reserve(ligne, Reserved_Langage_Word'Image(Debut))) then -- Si la ligne commence par un mot réservé
+        if (ligne_commence_par_mot_reserve
+               (ligne, Reserved_Langage_Word'Image (Debut)))
+        then -- Si la ligne commence par un mot réservé
             raise Aucune_Variable_Definie;  -- On lève une exception car cela indique qu'il n'y a pas de variable
         end if;
 
         loop
-            if (est_ligne_utile(ligne)) then  -- Si la ligne est utile (ie. pas un commentaire )
-                recuperer_variables(variables, ligne);   -- Récupération des variables
+            if (est_ligne_utile (ligne))
+            then  -- Si la ligne est utile (ie. pas un commentaire )
+                recuperer_variables
+                   (variables, ligne);   -- Récupération des variables
             end if;
-            ligne := renvoyer_ligne_sans_espace(Ada.Text_IO.Unbounded_IO.get_line(f)); -- Lecture de la ligne courante sans espace
-        exit when (ligne_commence_par_mot_reserve(ligne, Reserved_Langage_Word'Image(Debut))); -- Jusqu'à arriver à 'Debut'
+            ligne :=
+               renvoyer_ligne_sans_espace
+                  (Ada.Text_IO.Unbounded_IO.get_line
+                      (f)); -- Lecture de la ligne courante sans espace
+            exit when
+               (ligne_commence_par_mot_reserve
+                   (ligne,
+                    Reserved_Langage_Word'Image
+                       (Debut))); -- Jusqu'à arriver à 'Debut'
         end loop;
 
-        ligne := renvoyer_ligne_sans_espace(Ada.Text_IO.Unbounded_IO.get_line(f));
+        ligne :=
+           renvoyer_ligne_sans_espace (Ada.Text_IO.Unbounded_IO.get_line (f));
 
         loop
-            if (est_ligne_utile(ligne)) then
-                recuperer_instructions(instructions, variables, ligne);  -- Récupération des instructions
+            if (est_ligne_utile (ligne)) then
+                recuperer_instructions
+                   (instructions, variables,
+                    ligne);  -- Récupération des instructions
             end if;
-            ligne := renvoyer_ligne_sans_espace(Ada.Text_IO.Unbounded_IO.get_line(f));
-        exit when (ligne_commence_par_mot_reserve(ligne, Reserved_Langage_Word'Image(Fin)));    -- Jusqu'à arriver à 'Fin'
+            ligne :=
+               renvoyer_ligne_sans_espace
+                  (Ada.Text_IO.Unbounded_IO.get_line (f));
+            exit when
+               (ligne_commence_par_mot_reserve
+                   (ligne,
+                    Reserved_Langage_Word'Image
+                       (Fin)));    -- Jusqu'à arriver à 'Fin'
         end loop;
 
-        Close(F);
+        Close (F);
 
     end parse_file;
 
-    function recuperer_numero_instruction_ligne (index : in out integer; ligne : in Unbounded_String) return integer is
-        
+    function recuperer_numero_instruction_ligne
+       (index : in out integer; ligne : in Unbounded_String) return integer
+    is
+
         numInstruction : Unbounded_String;
-    
+
     begin
 
-        recuperer_chaine(numInstruction, ligne, index, 1);
-        return Integer'Value(To_String(numInstruction));
+        recuperer_chaine (numInstruction, ligne, index, 1);
+        return Integer'Value (To_String (numInstruction));
 
     end recuperer_numero_instruction_ligne;
 
-    procedure recuperer_instructions(instructions : in out T_List_Instruction; variables : in out T_List_Variable; ligne : in Unbounded_string) is
-     
-        i : integer;
-        nomVariableZ : Unbounded_String;
-        nomVariableX : Unbounded_String;
+    procedure recuperer_instructions
+       (instructions : in out T_List_Instruction;
+        variables    : in out T_List_Variable; ligne : in Unbounded_string)
+    is
+
+        i             : integer;
+        nomVariableZ  : Unbounded_String;
+        nomVariableX  : Unbounded_String;
         typeVariableZ : Unbounded_String;
-        nomVariableY : Unbounded_String;
-        operation : Unbounded_String;
-        instruction : T_Ptr_Instruction; 
-        isCaractere : boolean := false;
+        nomVariableY  : Unbounded_String;
+        operation     : Unbounded_String;
+        instruction   : T_Ptr_Instruction;
+        isCaractere   : boolean := false;
 
     begin
 
         instruction := new T_Instruction;
 
         -- recuperation du numero de l'instruction
-        i := 1;
-        instruction.all.numInstruction := recuperer_numero_instruction_ligne(i,ligne);
+        i                              := 1;
+        instruction.all.numInstruction :=
+           recuperer_numero_instruction_ligne (i, ligne);
 
-        if (i <= length(ligne)-1 and then Slice(ligne, i, i+1) = "IF") then
-            if_operation(ligne, i, instruction, operation, variables);
-        elsif (i <= length(ligne)-3 and then Slice(ligne, i, i+3) = "GOTO") then
-            goto_operation(ligne, i, instruction, operation, variables);
-        elsif (i <= length(ligne)-3 and then Slice(ligne, i, i+3) = "NULL") then
-            null_operation(operation);
-        elsif (i <= length(ligne)-5 and then Slice(ligne, i, i+5) = "Ecrire") then
-            ecrire_operation(ligne, i, instruction, operation, variables);
-        elsif (i <= length(ligne)-3 and then Slice(ligne, i, i+3) = "Lire") then
-            lire_operation(ligne, i, instruction, operation, variables);
+        if (i <= length (ligne) - 1 and then Slice (ligne, i, i + 1) = "IF")
+        then
+            if_operation (ligne, i, instruction, operation, variables);
+        elsif
+           (i <= length (ligne) - 3 and then Slice (ligne, i, i + 3) = "GOTO")
+        then
+            goto_operation (ligne, i, instruction, operation, variables);
+        elsif
+           (i <= length (ligne) - 3 and then Slice (ligne, i, i + 3) = "NULL")
+        then
+            null_operation (operation);
+        elsif
+           (i <= length (ligne) - 5
+            and then Slice (ligne, i, i + 5) = "Ecrire")
+        then
+            ecrire_operation (ligne, i, instruction, operation, variables);
+        elsif
+           (i <= length (ligne) - 3 and then Slice (ligne, i, i + 3) = "Lire")
+        then
+            lire_operation (ligne, i, instruction, operation, variables);
         else
-            affectation_operation(ligne, i, instruction, operation, variables);
+            affectation_operation
+               (ligne, i, instruction, operation, variables);
         end if;
 
         instruction.all.operation := operation;
-        ajouter(instructions, instruction);
+        ajouter (instructions, instruction);
 
     end recuperer_instructions;
 
-    procedure interpreter_commande (instructions : in out T_List_Instruction; variables : in out T_List_Variable) is
-        
-        nomOperation : Unbounded_String;
-        nomVariable : Unbounded_String;
-        nomIndice : Unbounded_String;
+    procedure interpreter_commande
+       (instructions : in out T_List_Instruction;
+        variables    : in out T_List_Variable)
+    is
+
+        nomOperation  : Unbounded_String;
+        nomVariable   : Unbounded_String;
+        nomIndice     : Unbounded_String;
         nomIndiceReel : Unbounded_String;
-        chaineLue : Unbounded_String;
-    
+        chaineLue     : Unbounded_String;
+
     begin
 
         nomOperation := instructions.all.ptrIns.all.operation;
 
-        if(nomOperation = "NULL") then
-            branchement_basic(instructions, instructions.all.ptrIns.all.numInstruction+1);
-        elsif(nomOperation = "GOTO") then
-            branchement_basic(instructions, instructions.all.ptrIns.all.operandes.z.all.valeurVariable);
-        elsif(nomOperation = "IF") then
-            branchement_conditionel(instructions, variables);
-        elsif(nomOperation = "ECRIRE") then
-            ecrire(instructions, variables);
-        elsif(nomOperation = "LIRE") then
-            lire(instructions, variables);
+        if (nomOperation = "NULL") then
+            branchement_basic
+               (instructions, instructions.all.ptrIns.all.numInstruction + 1);
+        elsif (nomOperation = "GOTO") then
+            -- On passe à l'instruction pointée par la valeur de la variable z de l'instruction ( GOTO z)
+            branchement_basic
+               (instructions,
+                instructions.all.ptrIns.all.operandes.z.all.valeurVariable);
+        elsif (nomOperation = "IF") then
+            branchement_conditionel (instructions, variables);
+        elsif (nomOperation = "ECRIRE") then
+            ecrire (instructions, variables);
+        elsif (nomOperation = "LIRE") then
+            lire (instructions, variables);
         else
-            affectation(instructions, variables);
+            affectation (instructions, variables);
         end if;
 
     end interpreter_commande;
--- src/interpreteur/intermediaire.ads
+++ src/interpreteur/intermediaire.ads
@@ -1,27 +1,27 @@
-with Ada.Strings.Unbounded;
-use Ada.Strings.Unbounded;
-with Ada.Command_Line; use Ada.Command_Line;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Ada.Command_Line;      use Ada.Command_Line;
 
 package intermediaire is
     -- Enumerations
-    type Reserved_Langage_Word is (Programme,Debut, Fin);
-     type Type_Defini is (Entier,Booleen); -- Soumis à modification pour la deuxième partie
-    Aucune_Variable_Definie : Exception; -- Aucune variable n'est definie dans le programme
-    Type_Incorrect : Exception; -- Le type de la variable n'est pas correct pour l'opération à effectuer
-    Variable_Non_Declaree : Exception; -- La variable à utiliser n'est pas declaree dans le programme
-    Variable_Deja_Definie : Exception; -- La variable a déjà été déclarée dans le programme
-    Variable_Non_Trouvee : Exception; -- Variable non trouvée dans la recherche d'une variable
-    Acces_Limite : Exception; -- Le compteur CP est hors limite (Ex : Programme avec un corps de 10 lignes et CP = 11) dans le programme
-    Fichier_Non_Lisible : Exception;
-    Fichier_Non_Trouve : Exception;
-    Element_Tableau_Deja_Utilise : Exception;
-    Instuction_Not_Found: Exception;
+    type Reserved_Langage_Word is (Programme, Debut, Fin);
+    type Type_Defini is
+       (Entier, Booleen); -- Soumis à modification pour la deuxième partie
+    Aucune_Variable_Definie : exception; -- Aucune variable n'est definie dans le programme
+    Type_Incorrect : exception; -- Le type de la variable n'est pas correct pour l'opération à effectuer
+    Variable_Non_Declaree : exception; -- La variable à utiliser n'est pas declaree dans le programme
+    Variable_Deja_Definie : exception; -- La variable a déjà été déclarée dans le programme
+    Variable_Non_Trouvee : exception; -- Variable non trouvée dans la recherche d'une variable
+    Acces_Limite : exception; -- Le compteur CP est hors limite (Ex : Programme avec un corps de 10 lignes et CP = 11) dans le programme
+    Fichier_Non_Lisible          : exception;
+    Fichier_Non_Trouve           : exception;
+    Element_Tableau_Deja_Utilise : exception;
+    Instuction_Not_Found         : exception;
 
-    type Tab_Chaines is array(1..2) of Unbounded_String;
+    type Tab_Chaines is array (1 .. 2) of Unbounded_String;
 
     type T_Chaines_Reservees is record
-        chaines : Tab_Chaines;
-        nbElements : integer;
+        chaines    : Tab_Chaines;
+        nbElements : Integer;
     end record;
 
     type T_Cell_Variable;
@@ -44,27 +44,27 @@ package intermediaire is
 
     type T_Variable is record
         valeurVariable : Integer;
-        typeVariable : Unbounded_String;
-        nomVariable : Unbounded_String;
-        isConstant : Boolean;
+        typeVariable   : Unbounded_String;
+        nomVariable    : Unbounded_String;
+        isConstant     : Boolean;
     end record;
 
     type T_Instruction is record
         numInstruction : Integer;
-        operandes : T_Operandes;
-        operation : Unbounded_String;
+        operandes      : T_Operandes;
+        operation      : Unbounded_String;
     end record;
 
     type T_Cell_Instruction is record
-        ptrIns : T_Ptr_Instruction; 
-        prev : T_List_Instruction;
-        next : T_List_Instruction;
+        ptrIns : T_Ptr_Instruction;
+        prev   : T_List_Instruction;
+        next   : T_List_Instruction;
     end record;
 
     type T_Cell_Variable is record
         ptrVar : T_Ptr_Variable;
-        next : T_List_Variable;
-        prev : T_List_Variable;
+        next   : T_List_Variable;
+        prev   : T_List_Variable;
     end record;
 
     -- Parse le fichier en lisant une par une les lignes du fichier afin de passer les données aux autres fonctions
@@ -77,9 +77,10 @@ package intermediaire is
     -- Postconditions
     --  aucune
     -- Exceptions : Aucune_Variable_Definie
-    procedure parse_file (fileName : in string; variables : in out T_List_Variable; instructions : in out T_List_Instruction);
+    procedure parse_file
+       (fileName     : in     String; variables : in out T_List_Variable;
+        instructions : in out T_List_Instruction);
 
-    
     -- Wrapper pour le traitement du programme
     -- Paramètres :
     --  fileName in string : le nom du fichier à lire
@@ -91,35 +92,29 @@ package intermediaire is
     --  Aucune
     procedure traiter_programme;
 
-
-    
     -- nom : recuperer_instructions
     -- semantique : récupère les lignes d'un programme en langage intermédiaire contenues entre 'Début' (exclu) et 'Fin' (exclue)
-    -- paramètres 
+    -- paramètres
     --  instructions : in out T_List_Instruction : la liste des instructions
     --  ligne : in Unbounded_string : la ligne à analyser
-    -- Préconditions : 
+    -- Préconditions :
     --                  le fichier est fermé
     --                  le contenu entre 'Début' et 'Fin' n'est pas vide
     -- Postconditions : le fichier est fermé
-    procedure recuperer_instructions(instructions : in out T_List_Instruction; variables : in out T_List_Variable; ligne : in Unbounded_string);
+    procedure recuperer_instructions
+       (instructions : in out T_List_Instruction;
+        variables    : in out T_List_Variable; ligne : in Unbounded_string);
 
     -- nom : interpreter_commande
-    -- semantique : interprete la ligne pointée par ptrLine
+    -- semantique : interprete la ligne pointée par instructions
     -- parametres :
     --  ptrLine : in out T_Cell_Instruction : la ligne à interpréter
     --  ptrVar : in out T_List_Variable : la liste des variables
     -- Préconditions    : ptrLigne n'est pas null
     -- Postconditions   : le numéro de la ligne pointée par ptrLine change pendant la procédure
     -- Exceptions       : Acces_Limite
-    procedure interpreter_commande (instructions : in out T_List_Instruction; variables : in out T_List_Variable);
-
-
-
-    
-
-   
-
-   
+    procedure interpreter_commande
+       (instructions : in out T_List_Instruction;
+        variables    : in out T_List_Variable);
 
 end intermediaire;
